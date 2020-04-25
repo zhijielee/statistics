c5a9c03f08b212404f4739799ec205e8cae6a36a
zhijielee <i@zhijie.li>
2020-04-26 01:15:58 +0800

    excel导出完成

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -5,10 +5,10 @@ namespace App\Http\Controllers;
 
 
 
-use Carbon\Exceptions\ParseErrorException;
 use Illuminate\Http\Request;
 use App\Http\Controllers\CommonController as Func;
 use Illuminate\Support\Facades\DB;
+use Maatwebsite\Excel\Excel as Excel;
 
 class IndexController extends Controller {
 
@@ -66,7 +66,7 @@ class IndexController extends Controller {
 //    }
 
 
-    public static function toExcel(Request $request) {
+    public static function toExcel(Request $request, Excel $excel) {
         if(is_null($request->input("name"))) {
             Func::alert("数据量过大，请选择导出");
             Func::goBack();
@@ -77,20 +77,35 @@ class IndexController extends Controller {
 //                "FROM t_qrcode Q INNER JOIN t_td_user U ON Q.USID = U.SID order by Q.SID desc) as T1 ".
 //                "inner join t_td_group_user GU on GU.U_SID = T1.sid) AS T2 ".
 //                "inner join t_td_group AS G ON G.SID = T2.GROUP_SID");
-        } else {
-            $name = $request->input("name");
-            $result = DB::select("SELECT G.TITLE AS group_name, T2.name as name, T2.name as name, T2.goin, T2.time as time ".
-                "FROM (select GU.GROUP_SID, T1.name as name, T1.goin as goin, T1.time as time ".
-                "from (SELECT U.SID AS sid, NAME AS name, Q.GOIN AS goin, Q.CREATE_TIME as time ".
-                "FROM t_qrcode Q INNER JOIN t_td_user U where U.NAME = ? and Q.USID = U.SID order by Q.SID) as T1 ".
-                "inner join t_td_group_user GU on GU.U_SID = T1.sid) AS T2 ".
-                "inner join t_td_group AS G ON G.SID = T2.GROUP_SID",[$name]);
         }
+        $name = $request->input("name");
+        $result = DB::select("SELECT G.TITLE AS group_name, T2.name as name, T2.name as name, T2.goin, T2.time as time ".
+            "FROM (select GU.GROUP_SID, T1.name as name, T1.goin as goin, T1.time as time ".
+            "from (SELECT U.SID AS sid, NAME AS name, Q.GOIN AS goin, Q.CREATE_TIME as time ".
+            "FROM t_qrcode Q INNER JOIN t_td_user U where U.NAME = ? and Q.USID = U.SID order by Q.SID) as T1 ".
+            "inner join t_td_group_user GU on GU.U_SID = T1.sid) AS T2 ".
+            "inner join t_td_group AS G ON G.SID = T2.GROUP_SID",[$name]);
 
-        dd($result);
+        $title = [
+            0 => '姓名',
+            1 => '时间',
+            2 => '进出类型',
+            3 => '单位'
+        ];
+        $export = null;
+        foreach ($result as $key => $val) {
+            $export[$key][0] = $val->name;
+            $export[$key][1] = $val->time;
+            $export[$key][2] = $val->goin;
+            $export[$key][3] = $val->group_name;
+        }
+        $sell = array_merge($title, $export);
+        $excel->create($name.'的活动记录',function($excel) use ($sell){
+            $excel->sheet('score', function($sheet) use ($sell){
+                $sheet->rows($sell);
+            });
+        })->export('xls');
         Func::goBack();
-        exit();
-//        Func::redirect("/index?name=".$name);
     }
 
 }

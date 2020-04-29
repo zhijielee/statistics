426623ad2353ab3adc4a1aedac4c8b158082968c
RLEX-T470P\rlex <hjpeng@tju.edu.cn>
2020-04-29 14:56:52 +0800

    咋感觉提交这么麻烦

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>TJU疫情防控数据平台</title>
+    <title>TJU-疫情防控数据平台</title>
 
     <!-- Bootstrap -->
     <link href="/gentelella/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
